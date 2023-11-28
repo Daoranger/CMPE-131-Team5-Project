eedb6b63dd37cd66228810300d71a885286a27bd
Daoranger <hoangnguyen11@DaLaptop>
2023-11-28 15:29:58 -0800

    fixed only one account log in at a time

--- app/routes.py
+++ app/routes.py
@@ -5,10 +5,10 @@ from .forms import LoginForm
 from .forms import CreateAccountForm
 from app import myapp_obj
 from flask import session
+from flask import request
 import re #from Python "Regular Expression Operations"
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import timedelta
-
 from app import db
 
 from flask_login import UserMixin
@@ -43,6 +43,11 @@ def dashboard():
 
 @myapp_obj.route("/login", methods=['GET', 'POST'])
 def login():
+
+    if 'user_id' in session:
+        flash('You are already logged in', 'info')
+        return redirect('/dashboard')
+        
     form = LoginForm()
     
     if form.validate_on_submit():
